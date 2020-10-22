0547e7d7a4900226812a1d8913f971ad61ac234f
cAttte <26514199+cAttte@users.noreply.github.com>
2020-10-22 02:06:13 -0300

    refactor: remove generateURL(), create generateID()

--- src/generateID.js
+++ src/generateID.js
@@ -1,8 +1,7 @@
 module.exports = function generateURL() {
     const characters = "abcdefghijklmnopqrstuvwxyz1234567890"
-    const id = Array(6)
+    return Array(6)
         .fill()
         .map(() => characters[Math.floor(Math.random() * characters.length)])
         .join("")
-    return `https://prnt.sc/${id}`
 }
