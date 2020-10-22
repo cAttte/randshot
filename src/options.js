0634baf6dbdcd31219a05e41f35b02cde5ebbbbf
cAttte <26514199+cAttte@users.noreply.github.com>
2020-10-22 02:11:27 -0300

    feat: create options()

---
+++ src/options.js
@@ -0,0 +1,37 @@
+const path = require("path")
+const fs = require("fs")
+const util = require("./util")
+
+module.exports = function options(program) {
+    program.option(
+        "-t, --timeout <time>",
+        "Time in milliseconds to wait between every download.",
+        input => {
+            input = Number(input)
+            if (isNaN(input)) util.error(`The {--timeout} option must be a number.`)
+            return input
+        },
+        1500
+    )
+
+    program.option(
+        "-o, --output <path>",
+        "The path where to save the images.",
+        input => {
+            input = path.resolve(input)
+            try {
+                fs.accessSync(input)
+            } catch {
+                try {
+                    fs.mkdirSync(input)
+                } catch {
+                    util.error(`Could not create {${input}}.`)
+                }
+            }
+            return input
+        },
+        "./output"
+    )
+
+    program.helpInformation = () => "help"
+}
