82aa710766f9a6077cbf83674b9a03b5c3439e64
cAttte <26514199+cAttte@users.noreply.github.com>
2020-10-22 02:19:25 -0300

    fix(util): use non-greedy selector

--- src/util.js
+++ src/util.js
@@ -3,12 +3,12 @@ const chalk = require("chalk")
 
 module.exports = {
     error(message, exit = true) {
-        message = message.replace(/\{(.+)\}/g, (_, $1) => chalk.redBright($1))
+        message = message.replace(/\{(.+?)\}/g, (_, $1) => chalk.redBright($1))
         console.log(symbols.error + " " + message)
         if (exit) process.exit()
     },
     success(message) {
-        message = message.replace(/\{(.+)\}/g, (_, $1) => chalk.greenBright($1))
+        message = message.replace(/\{(.+?)\}/g, (_, $1) => chalk.greenBright($1))
         console.log(symbols.success + " " + message)
     }
 }
